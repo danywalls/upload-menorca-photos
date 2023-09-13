75f35b83c69d678911219600f93390a377d876a3
Dany <danywalls4@gmail.com>
2023-09-13 18:39:54 +0200

    refactor: :recycle: refactor code using kendo filelist

--- src/app/components/image-upload/image-upload.component.ts
+++ src/app/components/image-upload/image-upload.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FileSelectComponent } from '@progress/kendo-angular-upload';
 import { EMPTY, catchError, tap } from 'rxjs';
 import { FakeImageUploadService } from 'src/app/services/fake-image-upload.service';
 
@@ -8,44 +9,20 @@ import { FakeImageUploadService } from 'src/app/services/fake-image-upload.servi
   styleUrls: ['./image-upload.component.css'],
 })
 export class ImageUploadComponent {
-  constructor(private fakeImageUploadService: FakeImageUploadService) {}
 
-  selectedImage!: FileList;
-  imagesUrl: Array<string> = [];
-  uploading = false;
+  imagesUrl: any = [];
 
-  onImageSelected(event: Event): void {
-    const inputElement = event.target as HTMLInputElement;
+  constructor(private fakeImageUploadService: FakeImageUploadService) { }
 
-    if (inputElement?.files && inputElement.files.length > 0) {
-      this.selectedImage = inputElement.files;
-    }
-  }
+  upload(pictureUploader: FileSelectComponent) {
 
-  uploadImage(): void {
-    if (this.selectedImage) {
-      this.uploadFiles(this.selectedImage);
-    }
-  }
-
-  private uploadFiles(images: FileList) {
-    this.uploading = true;
-    for (let index = 0; index < images.length; index++) {
-      const element = images[index];
-      this.fakeImageUploadService.uploadImage(element).subscribe((p) => {
-        this.imagesUrl.push(p);
-        this.uploading = false;
-      });
-    }
-  }
-
-  onDrop(event: DragEvent): void {
-    event.preventDefault();
-    if (event?.dataTransfer?.files) {
-      this.uploadFiles(event.dataTransfer.files);
-    }
-  }
-  onDragOver(event: DragEvent): void {
-    event.preventDefault();
+    pictureUploader.fileList.files.forEach(
+      p => {
+        const imageFile = p[0].rawFile
+        if (imageFile)
+          this.fakeImageUploadService.uploadImage(imageFile).subscribe(response => {
+            this.imagesUrl.push(response);
+          });
+      })
   }
 }
